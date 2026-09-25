0442cf7e6f1a2168a4249f491f15c39c799d2c03
workdayr <99066267+workdayr@users.noreply.github.com>
2026-09-25 22:40:50 +0600

    ROP lab rebuilded

--- Laboratorio_ROP_archivos/soluciones/a0_sol.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# A0: verificacion de entorno (no exploit)
-from pwn import process
-io = process("./bin/a0")
-io.sendline(b"hola")
-print(io.recvall(timeout=2).decode())
--- Laboratorio_ROP_archivos/soluciones/a1_sol.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# A1: primer crash con 100 'A'
-from pwn import process
-io = process("./bin/a1")
-io.sendline(b"A" * 100)
-io.wait()
-print("exit code:", io.returncode)  # esperado: -11 (SIGSEGV)
